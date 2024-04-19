4f59da9d35eda8b0296b40bf8d08e29764a92b1b
Régix <setondji.mededji@etu.univ-amu.Fr>
2024-04-19 14:57:19 +0200

    creation des entités

--- src/main/java/myapp/model/Poll.java
+++ src/main/java/myapp/model/Poll.java
@@ -1,4 +1,37 @@
 package myapp.model;
 
-public class Poll {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Poll implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String location;
+
+    private Date limitDate;
+
+    @OneToMany(mappedBy = "poll", fetch = FetchType.EAGER, cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE })
+    private List<Slot> slots;
+
+    @OneToMany(mappedBy = "poll", fetch = FetchType.EAGER, cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE })
+    private List<User> creator;
+
+
+
 }
--- src/main/java/myapp/model/Slot.java
+++ src/main/java/myapp/model/Slot.java
@@ -1,4 +1,28 @@
 package myapp.model;
 
-public class Slot {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Slot implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private boolean available;
+
+
 }
