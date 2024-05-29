3d2d53e2d52b04ae047dc356412c680e73f32cf0
vincent <vincent.nze@etu.univ-amu.fr>
2024-05-29 17:45:32 +0200

    Redirection vers dashboard après choix du crénaux

--- src/main/java/myapp/controller/EmailController.java
+++ src/main/java/myapp/controller/EmailController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,15 +20,15 @@ public class EmailController {
     private MailService mailService;
 
     @GetMapping("/send-mail")
-    public String sendMail(@RequestParam String senderEmail,
-                           @RequestParam String recipientEmail,
-                           @RequestParam String text) {
+    public ModelAndView sendMail(@RequestParam String senderEmail,
+                                 @RequestParam String recipientEmail,
+                                 @RequestParam String text) {
         User sender = new User();
         sender.setEmail(senderEmail);
         String subject = "Créneaux choisis ";
         // Split the recipient email addresses by comma and convert to a list
         List<String> recipients = Arrays.asList(recipientEmail.split(","));
         mailService.sendEmail(sender, recipients, subject, text);
-        return "Email sent!";
+        return new ModelAndView("redirect:/dashboard");
     }
 }
